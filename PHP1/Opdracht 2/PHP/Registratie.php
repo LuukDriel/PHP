7444199fb8a75b7fbf28da9ba2428c43e73abbe9
LuukDriel <166855482+LuukDriel@users.noreply.github.com>
2025-01-21 13:48:27 +0100

    PHP opdr 2

---
+++ PHP1/Opdracht 2/PHP/Registratie.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <form action="Registratie_verwerk.php" method="post" class="container mt-5">
+        <div class="form-group">
+            <label for="naam">naam:</label>
+            <input type="text" class="form-control" name="naam" id="naam" required>
+        </div>
+        <div class="form-group">
+            <label for="email">E-mail:</label>
+            <input type="email" class="form-control" name="email" id="email" required>
+        </div>
+        <div class="form-group">
+            <label for="wachtwoord">Wachtwoord:</label>
+            <input type="password" class="form-control" name="wachtwoord" id="wachtwoord" required>
+            <div class="invalid-feedback">
+                Wachtwoord moet minimaal 8 tekens lang zijn
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="wachtwoord_herhaal">Herhaal wachtwoord:</label>
+            <input type="password" class="form-control" name="wachtwoord_herhaal" id="wachtwoord_herhaal" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Registreer</button>
+    </form>
+</body>
+</html>
---
+++ PHP1/Opdracht 2/PHP/Registratie_verwerk.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $naam = $_POST["naam"];
+    $email = $_POST["email"];
+    $wachtwoord = $_POST["wachtwoord"];
+    $wachtwoord_herhaal = $_POST["wachtwoord_herhaal"];
+
+    if ($wachtwoord !== $wachtwoord_herhaal) {
+        echo "Wachtwoorden komen niet overeen";
+    exit;
+    }
+
+    if (strlen($wachtwoord) <8 ) {
+        echo "Wachtwoord moet minimaal 8 tekens lang zijn";
+        exit;
+    }
+
+    $hased_password = password_hash($wachtwoord, PASSWORD_DEFAULT);
+
+    $registratie = [
+        'naam' => $naam,
+        'email' => $email,
+        'wachtwoord' => $hashedWachtwoord,
+    ];
+
+    echo "Registratie gelukt! <br>";
+    echo "Naam: " . $registratie['naam'] . "<br>";
+    echo "E-mail: " . $registratie['email'] . "<br>";
+}
+?>
